555aebad15b8c12edd4f37beec0f1ef578f564f0
Yuri Soares <yurisoaresm@outlook.com>
2022-03-30 15:11:19 -0300

    initial commit forms1

---
+++ javascript/forms1/script.js
@@ -0,0 +1,10 @@
+function calculaArea() {
+  let h2o = document.getElementById("out");
+  let inR = document.getElementById("r");
+  let raio = inR.value;
+  
+  let area = 3.14 * raio ** 2;
+  
+  h2o.innerHTML = "Área para raio "+ raio +" = " + area;
+}
+
