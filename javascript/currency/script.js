c7222ce56bd200c385d5f57ff2adb8acd23eef93
Yuri Soares <yurisoaresm@outlook.com>
2022-04-13 14:02:30 -0300

    initial commit currency

---
+++ javascript/currency/script.js
@@ -0,0 +1,35 @@
+function convert() {
+  var moedaIn = document.querySelector('input[name="in"]:checked')
+  var min = moedaIn.value
+  
+  var moedaOut = document.querySelector('input[name="out"]:checked')
+  var mout = moedaOut.value
+
+  var input = document.getElementById("amount")
+  var valorOriginal = input.value
+  
+  var valorEmReal = 0.0
+  if (min == "dolar") {
+    valorEmReal = valorOriginal * 4.67
+  } else if (min == "euro") {
+    valorEmReal = valorOriginal * 5.06
+  } else if (min == "libra") {
+    valorEmReal = valorOriginal * 6.08
+  } else {
+    valorEmReal = valorOriginal
+  }
+
+  let valorConvertido = 0.0
+  if (mout == "dolar") {
+    valorConvertido = valorEmReal * 0.21
+  } else if (mout == "euro") {
+    valorConvertido = valorEmReal * 0.20
+  } else if (mout == "libra") {
+    valorConvertido = valorEmReal * 0.16
+  } else {
+    valorConvertido = valorEmReal
+  }
+  
+  var spanOut = document.getElementById("result")
+  spanOut.innerText = valorConvertido
+}
